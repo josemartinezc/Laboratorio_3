a64aed5543f49f3c8e8f000fb824d205997fc1df
josemartinezc <josefinamcalero@gmail.com>
2019-12-14 07:31:58 -0300

    estable 3

---
+++ build/default/production/SIM808/GSM.o.d
@@ -0,0 +1,11 @@
+build/default/production/SIM808/GSM.o:  \
+SIM808/GSM.c  \
+SIM808/GSM.h  \
+ SIM808/../SIM808/SIM808.h  \
+SIM808/../SIM808/../utils/utils.h  \
+ SIM808/../Irrigation_system/system_control.h  \
+ SIM808/../Irrigation_system/../mcc_generated_files/rtcc.h  \
+ SIM808/../Irrigation_system/../periferics/sensor.h  \
+ SIM808/../Irrigation_system/../utils/utils.h  \
+SIM808/GPS.h  \
+ SIM808/../utils/utils.h 
